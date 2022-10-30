7ee3d8ae0329adcbd0488c168feb7a0d9ac27714
Obiwac <obiwac@gmail.com>
2022-10-30 23:27:26 +0100

    bob: Move building code out of main file
    Now, 'instr.h' is where all the instruction-specific code lives, and
    where all the classes are included from.

---
+++ src/instr.h
@@ -0,0 +1,112 @@
+#pragma once
+
+#include "base/base.h"
+
+#include "classes/cc.h"
+#include "classes/file.h"
+#include "classes/linker.h"
+
+static WrenForeignMethodFn wren_bind_foreign_method(WrenVM* wm, char const* module, char const* class, bool static_, char const* signature) {
+	WrenForeignMethodFn fn = unknown_foreign;
+
+	// classes
+
+	if (!strcmp(class, "CC")) {
+		fn = cc_bind_foreign_method(static_, signature);
+	}
+
+	else if (!strcmp(class, "File")) {
+		fn = file_bind_foreign_method(static_, signature);
+	}
+
+	else if (!strcmp(class, "Linker")) {
+		fn = linker_bind_foreign_method(static_, signature);
+	}
+
+	// unknown
+
+	if (fn == unknown_foreign) {
+		LOG_WARN("Unknown%s foreign method '%s' in module '%s', class '%s'", static_ ? " static" : "", signature, module, class)
+	}
+
+	return fn;
+}
+
+static WrenForeignClassMethods wren_bind_foreign_class(WrenVM* wm, char const* module, char const* class) {
+	WrenForeignClassMethods meth = { NULL };
+
+	if (!strcmp(class, "CC")) {
+		meth.allocate = cc_new;
+		meth.finalize = cc_del;
+	}
+
+	else if (!strcmp(class, "Linker")) {
+		meth.allocate = linker_new;
+		meth.finalize = linker_del;
+	}
+
+	else {
+		LOG_WARN("Unknown foreign class '%s' in module '%s'", class, module)
+	}
+
+	return meth;
+}
+
+static int do_build(void) {
+	// setup wren virtual machine
+
+	WrenConfiguration config;
+	wrenInitConfiguration(&config);
+
+	config.writeFn = &wren_write_fn;
+	config.errorFn = &wren_error_fn;
+
+	config.bindForeignMethodFn = &wren_bind_foreign_method;
+	config.bindForeignClassFn  = &wren_bind_foreign_class;
+
+	WrenVM* vm = wrenNewVM(&config);
+
+	// read build configuration base file
+
+	char const* const module = "main";
+	WrenInterpretResult result = wrenInterpret(vm, module, base_src);
+
+	if (result == WREN_RESULT_SUCCESS) {
+		LOG_SUCCESS("Build configuration base ran successfully")
+	}
+
+	// read build configuration file
+
+	FILE* fp = fopen("build.wren", "r");
+
+	if (!fp) {
+		LOG_FATAL("Couldn't read 'build.wren'")
+		return EXIT_FAILURE;
+	}
+
+	fseek(fp, 0, SEEK_END);
+	size_t bytes = ftell(fp);
+	rewind(fp);
+
+	char* config_src = malloc(bytes);
+
+	if (fread(config_src, 1, bytes, fp))
+		;
+
+	config_src[bytes - 1] = 0;
+
+	result = wrenInterpret(vm, module, config_src);
+
+	if (result == WREN_RESULT_SUCCESS) {
+		LOG_SUCCESS("Build configuration ran successfully")
+	}
+
+	// clean everything up (not super necessary, I'd put a little more effort in error handling if it was :P)
+
+	wrenFreeVM(vm);
+
+	free(config_src);
+	fclose(fp);
+
+	return EXIT_SUCCESS;
+}
--- src/main.c
+++ src/main.c
@@ -25,57 +25,11 @@ static char* bin_path = NULL;
 
 #include "util.h"
 
-#include "base/base.h"
-
 #include "classes/cc.h"
 #include "classes/file.h"
 #include "classes/linker.h"
 
-static WrenForeignMethodFn wren_bind_foreign_method(WrenVM* wm, char const* module, char const* class, bool static_, char const* signature) {
-	WrenForeignMethodFn fn = unknown_foreign;
-
-	// classes
-
-	if (!strcmp(class, "CC")) {
-		fn = cc_bind_foreign_method(static_, signature);
-	}
-
-	else if (!strcmp(class, "File")) {
-		fn = file_bind_foreign_method(static_, signature);
-	}
-
-	else if (!strcmp(class, "Linker")) {
-		fn = linker_bind_foreign_method(static_, signature);
-	}
-
-	// unknown
-
-	if (fn == unknown_foreign) {
-		LOG_WARN("Unknown%s foreign method '%s' in module '%s', class '%s'", static_ ? " static" : "", signature, module, class)
-	}
-
-	return fn;
-}
-
-static WrenForeignClassMethods wren_bind_foreign_class(WrenVM* wm, char const* module, char const* class) {
-	WrenForeignClassMethods meth = { NULL };
-
-	if (!strcmp(class, "CC")) {
-		meth.allocate = cc_new;
-		meth.finalize = cc_del;
-	}
-
-	else if (!strcmp(class, "Linker")) {
-		meth.allocate = linker_new;
-		meth.finalize = linker_del;
-	}
-
-	else {
-		LOG_WARN("Unknown foreign class '%s' in module '%s'", class, module)
-	}
-
-	return meth;
-}
+#include "instr.h"
 
 static char const* init_name = "bob";
 
@@ -125,20 +79,6 @@ int main(int argc, char* argv[]) {
 
 	if (!argc) {
 		usage();
-	}
-
-	// parse instructions
-
-	while (argc --> 0) {
-		char const* const instr = *argv;
-
-		if (!strcmp(instr, "build")) {
-			// XXX do_build();
-		}
-
-		else {
-			usage();
-		}
 	}
 
 	// make sure output directory exists
@@ -206,60 +146,28 @@ int main(int argc, char* argv[]) {
 		errx(EXIT_FAILURE, "realpath(\"%s\"): %s", _bin_path, strerror(errno));
 	}
 
-	// setup wren virtual machine
+	// parse instructions
 
-	WrenConfiguration config;
-	wrenInitConfiguration(&config);
+	while (argc --> 0) {
+		char const* const instr = *argv;
+		int rv = EXIT_FAILURE; // I'm a pessimist
 
-	config.writeFn = &wren_write_fn;
-	config.errorFn = &wren_error_fn;
+		if (!strcmp(instr, "build")) {
+			rv = do_build();
+		}
 
-	config.bindForeignMethodFn = &wren_bind_foreign_method;
-	config.bindForeignClassFn  = &wren_bind_foreign_class;
+		else {
+			usage();
+		}
 
-	WrenVM* vm = wrenNewVM(&config);
+		// stop here if there was an error in the execution of an instruction
 
-	// read build configuration base file
-
-	char const* const module = "main";
-	WrenInterpretResult result = wrenInterpret(vm, module, base_src);
-
-	if (result == WREN_RESULT_SUCCESS) {
-		LOG_SUCCESS("Build configuration base ran successfully")
+		if (rv != EXIT_SUCCESS) {
+			return rv;
+		}
 	}
 
-	// read build configuration file
-
-	FILE* fp = fopen("build.wren", "r");
-
-	if (!fp) {
-		LOG_FATAL("Couldn't read 'build.wren'")
-		return EXIT_FAILURE;
-	}
-
-	fseek(fp, 0, SEEK_END);
-	size_t bytes = ftell(fp);
-	rewind(fp);
-
-	char* config_src = malloc(bytes);
-
-	if (fread(config_src, 1, bytes, fp))
-		;
-
-	config_src[bytes - 1] = 0;
-
-	result = wrenInterpret(vm, module, config_src);
-
-	if (result == WREN_RESULT_SUCCESS) {
-		LOG_SUCCESS("Build configuration ran successfully")
-	}
-
-	// clean everything up (not super necessary, I'd put a little more effort in error handling if it was :P)
-
-	wrenFreeVM(vm);
-
-	free(config_src);
-	fclose(fp);
+	// if all went well, we may rest peacefully
 
 	return EXIT_SUCCESS;
 }
