b038ca37c9a4ffb15423762290c8fb3023fad87d
obiwac <obiwac@gmail.com>
2022-12-04 08:24:41 +0100

    OS.name: Detect if running under WSL (I hate you Microsoft for making this so dumb)

--- src/classes/os.h
+++ src/classes/os.h
@@ -33,6 +33,25 @@ static void os_name(WrenVM* vm) {
 		errx(EXIT_FAILURE, "uname: %s", strerror(errno));
 	}
 
+	// check if running under WSL (only on Linux)
+	// I actually hate this, how tf is there no surefire way of detecting if running under WSL
+	// how can you be so utterly incompetent at literally everything you do Microsoft
+	// https://github.com/microsoft/WSL/issues/4071
+
+#if defined(__linux__)
+	static bool wsl = false;
+
+	// first, check the release name information
+
+	wsl |= !!strstr(name.release, "Microsoft"); // likely WSL1
+	wsl |= !!strstr(name.release, "microsoft"); // likely WSL2
+
+	// then, idk, check for these environment variables
+
+	wsl |= !!getenv("WSL_INTEROP");
+	wsl |= !!getenv("WSL_DISTRO_NAME");
+#endif
+
 	// all of this is cached so we don't have to call 'uname' as often
 
 	cached = true;
@@ -40,6 +59,12 @@ static void os_name(WrenVM* vm) {
 cached:
 
 	os_name_add(vm, name.sysname);
+
+#if defined(__linux__)
+	if (wsl) {
+		os_name_add(vm, "WSL");
+	}
+#endif
 }
 
 // foreign method binding
