0894b2fff7b9ad2a3f280a6ab39cc05246ce8ad5
obiwac <obiwac@gmail.com>
2023-04-11 19:08:35 +0200

    build: Include `src` in include paths

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ mkdir -p sh-bin # 'bin' reserved for when compiling bob with bob
 # compile all objects
 
 cc_flags="
-	-std=c99 -g -O0 -Isrc/wren/include
+	-std=c99 -g -O0 -Isrc/wren/include -Isrc
 	-isystem=/usr/local/include -L/usr/local/lib
 	-DWREN_OPT_META=0 -DWREN_OPT_RANDOM=0
 	-Wno-unused-command-line-argument
--- build.wren
+++ build.wren
@@ -3,8 +3,9 @@
 var cc = CC.new()
 
 cc.add_opt("-std=c99")
-cc.add_opt("-isystem=/usr/local/include")
+cc.add_opt("-Isrc")
 cc.add_opt("-Isrc/wren/include")
+cc.add_opt("-isystem=/usr/local/include")
 cc.add_opt("-DWREN_OPT_META=0")
 cc.add_opt("-DWREN_OPT_RANDOM=0")
 cc.add_opt("-Wall")
