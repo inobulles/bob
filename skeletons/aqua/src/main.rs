2fa161c09d54efcc72be2e5de92e2321706716c5
obiwac <obiwac@gmail.com>
2023-07-22 11:48:50 +0200

    skeletons/aqua: Update example code with draw loop

--- skeletons/aqua/src/main.rs
+++ skeletons/aqua/src/main.rs
@@ -3,8 +3,9 @@ extern crate aqua;
 #[no_mangle]
 pub fn main() {
 	let mut win = aqua::win::Win::new(800, 600);
-
 	win.caption("Bob AQUA Skeleton");
 
-	std::thread::sleep(std::time::Duration::from_millis(3000));
+	win.draw_loop(|| {
+		0
+	});
 }
