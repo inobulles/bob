7dfa42c77094095edebcf0c5d1ab63dfbcd112af
Aymeric Wibo <obiwac@gmail.com>
2024-10-27 23:06:37 +0100

    build: Fix `intercept-build` on macOS

--- build.sh
+++ build.sh
@@ -1,24 +1,31 @@
 #!/bin/sh
 set -xe
 
+if [ -z "$CC" ]; then
+	CC=cc
+fi
+
 mkdir -p sh-bin # 'bin' reserved for when compiling bob with bob
 
 # compile all objects
 # TODO Should we have to -Isrc/flamingo? Can it be made to just use quote includes?
 
 cc_flags="
-	-std=c99 -g -O0 -Isrc -Isrc/flamingo
+	-std=c11 -g -O0 -Isrc
 	-isystem=/usr/local/include -L/usr/local/lib
-	-Wall -Wextra -Werror -Wno-unused-parameter -Wno-unused-variable
+	-Wall -Wextra -Werror -Wno-unused-parameter
 	-Wno-unused-command-line-argument
 	-lm
 "
 
-for src in $(find src -name "*.c" -type f); do
+srcs=$(find src -name "*.c" -o -path src/flamingo -prune -type f)
+srcs="$srcs src/flamingo/flamingo.c"
+
+for src in $srcs; do
 	mkdir -p sh-bin/$(dirname $src)
 
 	obj=sh-bin/$src.o
-	cc $cc_flags -c $src -o $obj &
+	$CC $cc_flags -c $src -o $obj &
 done
 
 wait
@@ -26,4 +33,4 @@ wait
 # link everything together
 
 objs=$(find sh-bin -name "*.o" -type f)
-cc $objs $cc_flags -o sh-bin/bob
+$CC $objs $cc_flags -o sh-bin/bob
