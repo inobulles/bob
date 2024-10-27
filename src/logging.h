5916b7b20f572dd5b9da5c2508287b02a5633b62
Aymeric Wibo <obiwac@gmail.com>
2024-10-27 23:10:07 +0100

    formatting: Format logging and common header

--- src/logging.h
+++ src/logging.h
@@ -22,8 +22,8 @@ typedef struct {
 
 void logging_init(void);
 
-__attribute__((__format__(__printf__, 3, 0)))
-void vlog(FILE* stream, char const* colour, char const* const fmt, ...);
+__attribute__((__format__(__printf__, 3, 0))) void
+vlog(FILE* stream, char const* colour, char const* const fmt, ...);
 
 // progress bar stuff
 
@@ -32,24 +32,24 @@ void progress_del(progress_t* self);
 
 void progress_complete(progress_t* self);
 
-__attribute__((__format__(__printf__, 4, 0)))
-void progress_update(progress_t* self, size_t numerator, size_t denominator, char const* fmt, ...);
+__attribute__((__format__(__printf__, 4, 0))) void
+progress_update(progress_t* self, size_t numerator, size_t denominator, char const* fmt, ...);
 
 // kinda replicate the umber API
 
-#define CLEAR   "\033[0m"
+#define CLEAR "\033[0m"
 #define REGULAR "\033[0;"
-#define BOLD    "\033[1;"
+#define BOLD "\033[1;"
 
-#define PURPLE  "35m"
-#define RED     "31m"
-#define YELLOW  "33m"
-#define GREEN   "32m"
-#define BLUE    "34m"
+#define PURPLE "35m"
+#define RED "31m"
+#define YELLOW "33m"
+#define GREEN "32m"
+#define BLUE "34m"
 
 #define REPLACE_LINE "\33[2K\r"
 
-#define LOG_FATAL(...)   vlog(stderr, "💀 " BOLD    PURPLE, __VA_ARGS__);
-#define LOG_ERROR(...)   vlog(stderr, "🔴 " BOLD    RED,    __VA_ARGS__);
-#define LOG_WARN(...)    vlog(stderr, "⚠️  " REGULAR YELLOW, __VA_ARGS__);
-#define LOG_SUCCESS(...) vlog(stderr, "🟢 " REGULAR GREEN,  __VA_ARGS__);
+#define LOG_FATAL(...) vlog(stderr, "💀 " BOLD PURPLE, __VA_ARGS__);
+#define LOG_ERROR(...) vlog(stderr, "🔴 " BOLD RED, __VA_ARGS__);
+#define LOG_WARN(...) vlog(stderr, "⚠️  " REGULAR YELLOW, __VA_ARGS__);
+#define LOG_SUCCESS(...) vlog(stderr, "🟢 " REGULAR GREEN, __VA_ARGS__);
