1634ab7388bc0edb7aac71836246b1b6bd3a645a
Aymeric Wibo <obiwac@gmail.com>
2024-10-30 12:05:39 +0100

    class/cc: Start implementing `CC.compile`

--- src/class/cc.c
+++ src/class/cc.c
@@ -9,18 +9,35 @@
 #include <fts.h>
 #include <stdio.h>
 
-static flamingo_val_t* compile_val = NULL;
+#define CC "CC"
 
-static void populate(char* key, size_t key_size, flamingo_val_t* val) {
-	// TODO This ain't gonna work cuz 'compile' isn't static.
+typedef struct {
+	flamingo_val_t* flags;
+} state_t;
 
-	if (flamingo_cstrcmp(key, "compile", key_size) == 0) {
-		compile_val = val;
+static int compile(flamingo_arg_list_t* args, flamingo_val_t** rv) {
+	if (args->count != 1) {
+		LOG_FATAL(CC ".compile: Expected 1 argument, got %zu", args->count);
+		return -1;
 	}
-}
 
-static int compile(flamingo_arg_list_t* args) {
-	printf("TODO compile\n");
+	if (args->args[0]->kind != FLAMINGO_VAL_KIND_VEC) {
+		LOG_FATAL(CC ".compile: Expected argument to be a vector");
+		return -1;
+	}
+
+	flamingo_val_t* const srcs = args->args[0];
+
+	for (size_t i = 0; i < srcs->vec.count; i++) {
+		flamingo_val_t* const src = srcs->vec.elems[i];
+
+		if (src->kind != FLAMINGO_VAL_KIND_STR) {
+			LOG_FATAL(CC ".compile: Expected %zu-th vector element to be a string", i);
+			return -1;
+		}
+
+		printf("TODO Compile source: %.*s\n", (int) src->str.size, src->str.str);
+	}
 
 	return 0;
 }
@@ -28,22 +45,29 @@ static int compile(flamingo_arg_list_t* args) {
 static int call(flamingo_val_t* callable, flamingo_arg_list_t* args, flamingo_val_t** rv, bool* consumed) {
 	*consumed = true;
 
-	if (callable == compile_val) {
-		return compile(args);
+	if (flamingo_cstrcmp(callable->name, "compile", callable->name_size) == 0) {
+		return compile(args, rv);
 	}
 
 	*consumed = false;
 	return 0;
 }
 
+static void free_state(flamingo_val_t* inst, void* data) {
+	state_t* const state = data;
+	free(state);
+}
+
 static int instantiate(flamingo_val_t* inst, flamingo_arg_list_t* args) {
+	// Validate flags argument.
+
 	if (args->count != 1) {
-		LOG_FATAL("CC: Expected 1 argument, got %zu", args->count);
+		LOG_FATAL(CC ": Expected 1 argument, got %zu", args->count);
 		return -1;
 	}
 
 	if (args->args[0]->kind != FLAMINGO_VAL_KIND_VEC) {
-		LOG_FATAL("CC: Expected argument to be a vector");
+		LOG_FATAL(CC ": Expected argument to be a vector");
 		return -1;
 	}
 
@@ -53,19 +77,27 @@ static int instantiate(flamingo_val_t* inst, flamingo_arg_list_t* args) {
 		flamingo_val_t* const flag = flags->vec.elems[i];
 
 		if (flag->kind != FLAMINGO_VAL_KIND_STR) {
-			LOG_FATAL("CC: Expected %zu-th vector element to be a string", i);
+			LOG_FATAL(CC ": Expected %zu-th vector element to be a string", i);
 			return -1;
 		}
-
-		printf("TODO CC flag: %.*s\n", (int) flag->str.size, flag->str.str);
 	}
+
+	// Create state object.
+
+	state_t* const state = malloc(sizeof *state);
+	assert(state != NULL);
+
+	state->flags = flags;
+
+	inst->inst.data = state;
+	inst->inst.free_data = free_state;
 
 	return 0;
 }
 
-bob_class_t const BOB_CLASS_CC = {
-	.name = "CC",
-	.populate = populate,
+bob_class_t BOB_CLASS_CC = {
+	.name = CC,
+	.populate = NULL,
 	.call = call,
 	.instantiate = instantiate,
 };
--- src/class/class.h
+++ src/class/class.h
@@ -7,15 +7,17 @@
 
 typedef struct {
 	char* name;
+	flamingo_val_t* class_val;
+
 	void (*populate)(char* key, size_t key_size, flamingo_val_t* val);
 	int (*call)(flamingo_val_t* callable, flamingo_arg_list_t* args, flamingo_val_t** rv, bool* consumed);
 	int (*instantiate)(flamingo_val_t* inst, flamingo_arg_list_t* args);
 } bob_class_t;
 
-extern bob_class_t const BOB_CLASS_FS;
-extern bob_class_t const BOB_CLASS_CC;
+extern bob_class_t BOB_CLASS_FS;
+extern bob_class_t BOB_CLASS_CC;
 
-static bob_class_t const* const BOB_CLASSES[] = {
+static bob_class_t* const BOB_CLASSES[] = {
 	&BOB_CLASS_FS,
 	&BOB_CLASS_CC,
 };
