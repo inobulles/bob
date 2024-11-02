3ced65139d9e54c6f3e8409c77073e16c15b7b65
Aymeric Wibo <obiwac@gmail.com>
2024-11-02 02:32:13 +0100

    cmd: Pipe output

--- src/cmd.h
+++ src/cmd.h
@@ -8,11 +8,17 @@
 typedef struct {
 	size_t len; // includes NULL sentinel
 	char** args;
+
+	// Pipe.
+
+	int in;
+	int out;
 } cmd_t;
 
-void cmd_create(cmd_t* cmd, ...);
+int cmd_create(cmd_t* cmd, ...);
 __attribute__((__format__(__printf__, 2, 3))) void cmd_addf(cmd_t* cmd, char const* fmt, ...);
 pid_t cmd_exec_async(cmd_t* cmd);
 int cmd_exec(cmd_t* cmd);
+char* cmd_read_out(cmd_t* cmd);
 void cmd_print(cmd_t* cmd);
 void cmd_free(cmd_t* cmd);
