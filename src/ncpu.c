0bffcb565fab63c063c14cea0c9af5aec9ebec5f
Aymeric Wibo <obiwac@gmail.com>
2024-11-03 16:00:24 +0100

    ncpu: Ability to set max jobs with `-j` flag

--- src/ncpu.c
+++ src/ncpu.c
@@ -8,6 +8,16 @@
 
 size_t max_jobs = 0;
 
+int set_max_jobs(size_t ncpu) {
+	if (ncpu == 0 || ncpu > 1024) {
+		LOG_FATAL("Invalid number of CPU's");
+		return -1;
+	}
+
+	max_jobs = ncpu;
+	return 0;
+}
+
 size_t ncpu(void) {
 	if (max_jobs > 0) {
 		return max_jobs;
