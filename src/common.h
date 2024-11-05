74076e71548688e0ebefe6c2d702905e54e70a45
Aymeric Wibo <obiwac@gmail.com>
2024-11-05 19:39:57 +0100

    flamingo: Update

--- src/common.h
+++ src/common.h
@@ -3,12 +3,12 @@
 
 #pragma once
 
-// this file must be included before anything else
-// feature test macros go here, because they're annoying and no one likes the
-// defaults see: feature_test_macros(7)
+// This file must be included before anything else.
+// Feature test macros go here, because they're annoying and no one likes the defaults.
+// See: feature_test_macros(7).
 
 #define __STDC_WANT_LIB_EXT2__ \
-	1 // ISO/IEC TR 24731-2:2010 standard library extensions
+	1 // ISO/IEC TR 24731-2:2010 standard library extensions.
 
 #if __linux__
 # define _GNU_SOURCE
