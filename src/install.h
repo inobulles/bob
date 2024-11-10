8ec354e6f7db76bbb8e42532b2124a9a30388e9a
Aymeric Wibo <obiwac@gmail.com>
2024-11-10 01:33:25 +0100

    linker: Better logging when already linked thanks to `cookie_to_output`

--- src/install.h
+++ src/install.h
@@ -7,4 +7,5 @@
 
 int setup_install_map(flamingo_t* flamingo, char const* prefix);
 int install_all(char const* prefix);
+char* cookie_to_output(char* cookie, flamingo_val_t** key_val_ref);
 int install_cookie(char* cookie);
