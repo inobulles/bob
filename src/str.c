5cab6f3ab5b411b5d5d498735401481e396fde23
Aymeric Wibo <obiwac@gmail.com>
2024-11-16 22:27:36 +0100

    str: Accept a `size` argument to `str_hash`
    Before, non-NUL-terminated strings were being passed to `str_hash`, which is obviously not right.

--- src/str.c
+++ src/str.c
@@ -3,11 +3,9 @@
 
 #include <stdlib.h>
 #include <str.h>
-#include <string.h>
 
-uint64_t str_hash(char const* str) { // djb2 algorithm.
+uint64_t str_hash(char const* str, size_t len) { // djb2 algorithm.
 	uint64_t hash = 5381;
-	size_t const len = strlen(str);
 
 	for (size_t i = 0; i < len; i++) {
 		hash = ((hash << 5) + hash) + str[i]; // hash * 33 + ptr[i]
