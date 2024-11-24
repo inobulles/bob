7f26405edc1823cf93c60b01620a7caefead9545
Aymeric Wibo <obiwac@gmail.com>
2024-11-24 20:18:17 +0100

    tests: Tests for `bob sh`

---
+++ tests/sh.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+. tests/common.sh
+
+# Test the 'bob sh' command.
+# Also kind of a regression test for the following PR:
+# https://github.com/inobulles/bob/pull/77
+
+if [ "$(bob sh echo test | tail -n1)" != "test" ]; then
+	echo "Failed to run command in shell." >&2
+	exit 1
+fi
+
+if [ "$(bob sh sh -c "echo \$PATH" | tail -n1 | cut -d':' -f1)" != ".bob/prefix/bin" ]; then
+	echo "Temporary installation prefix not in \$PATH." >&2
+	exit 1
+fi
+
+# XXX Can't really test 'bob sh' on it's own "properly" I don't think.
+
+export SHELL=uname
+
+if [ "$(bob sh | tail -n1)" != "$(uname)" ]; then
+	echo "Failed to run command in shell." >&2
+	exit 1
+fi
