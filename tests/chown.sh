d8d8dba8c9b91932ff7888ce10991de28c36444e
Aymeric Wibo <obiwac@gmail.com>
2024-11-24 15:36:56 +0100

    tests: Add chown regression test for #69

---
+++ tests/chown.sh
@@ -0,0 +1,47 @@
+#!/bin/sh
+set -e
+
+# Regression test for the following PR:
+# https://github.com/inobulles/bob/pull/69
+# Basically, there's this feature which chown's generated files to the owner of the project directory when running Bob as root.
+# The issue though is that, before #69, even installed files would be chown'd to the owner of the project directory.
+# The result was that on FreeBSD, the ldconfig service would refuse to cache the libraries in /usr/local/lib because they weren't owned by root, which is obviously very bad as nothing that linked against those would work.
+
+# Make sure everything is clean.
+
+INSTALL_PATH=/usr/local/share/bob_chown_test.fl
+$SUDO rm -rf tests/chown/.bob $INSTALL_PATH
+
+# Create a user.
+
+USER=bobtestuser
+
+if [ $(uname) == "Linux" ] || [ $(uname) == "FreeBSD" ]; then
+	useradd -m $USER
+elif [ $(uname) == "Darwin" ]; then
+	$SUDO sysadminctl -addUser $USER
+else
+	echo "Unsupported OS (don't know how to create a user)."
+	exit 1
+fi
+
+# Chown the project directory to that user.
+
+$SUDO chown -R bobtestuser tests/chown
+$SUDO .bootstrap/bob -C tests/chown build
+
+if [ ! -z "$(find tests/chown ! -user bobtestuser)" ]; then
+	echo "Some files are not owned by bobtestuser in tests/chown."
+	exit 1
+fi
+
+$SUDO .bootstrap/bob -C tests/chown install
+
+# XXX Annoyingly, 'stat -f %Su' doesn't work on Linux (you have to use 'stat -c %U').
+
+owner=$(ls -ld /usr/local/share/bob_chown_test.fl | awk '{print $3}')
+
+if [ $owner != "root" ]; then
+	echo "Installed files are not owned by root."
+	exit 1
+fi
