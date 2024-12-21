ad9dd18a3cadb36f8799e17ca8c42170dd4f5bee
Aymeric Wibo <obiwac@gmail.com>
2024-12-21 22:30:40 +0100

    tests/deps: Remove no preinstallation test, which is wrong with the new preinstallation rules

--- tests/deps.sh
+++ tests/deps.sh
@@ -16,25 +16,3 @@ if [ $? != 0 ]; then
 	echo "Failed to run binary pre-installed by dependency: $out" >&2
 	exit 1
 fi
-
-# If we're just building, make sure nothing is pre-installed to the temporary installation prefix for no reason.
-
-cp tests/deps/build.normal.fl tests/deps/build.fl
-rm -rf tests/deps/.bob tests/deps/dep1/.bob tests/deps/dep2/.bob
-
-out=$(bob -C tests/deps build 2>&1)
-
-if [ $? != 0 ]; then
-	echo "Failed to build: $out" >&2
-	exit 1
-fi
-
-if [ -f tests/deps/.bob/prefix/bin/dep2 ]; then
-	echo "Dependency binary has been installed to temporary installation prefix when it shouldn't've." >&2
-	exit 1
-fi
-
-if [ -f tests/deps/dep2/.bob/prefix/bin/dep2 ]; then
-	echo "Dependency binary has been installed to own temporary installation prefix when it shouldn't've." >&2
-	exit 1
-fi
