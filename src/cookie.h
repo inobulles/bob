bbdeeb57b314df95c0c8737bd7751829b910d610
Aymeric Wibo <obiwac@gmail.com>
2024-12-24 20:30:06 +0100

    install: Record built cookies

--- src/cookie.h
+++ src/cookie.h
@@ -3,6 +3,14 @@
 
 #pragma once
 
+#include <pthread.h>
+#include <stdbool.h>
 #include <stdlib.h>
 
+extern size_t built_cookie_count;
+extern char** built_cookies;
+
 char* gen_cookie(char* path, size_t path_size, char const* ext);
+
+void add_built_cookie(char* cookie);
+bool has_built_cookie(char* cookie, size_t len);
