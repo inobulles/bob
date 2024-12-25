1ad0a061707ae6e5bbfe9ad2a90aafc111eed88b
Aymeric Wibo <obiwac@gmail.com>
2024-12-25 10:02:36 +0100

    aquarium: Boilerplate for `Aquarium` class implementation

--- src/class/class.h
+++ src/class/class.h
@@ -14,6 +14,7 @@ typedef struct {
 	int (*instantiate)(flamingo_val_t* inst, flamingo_arg_list_t* args);
 } bob_class_t;
 
+extern bob_class_t BOB_CLASS_AQUARIUM;
 extern bob_class_t BOB_CLASS_CC;
 extern bob_class_t BOB_CLASS_CARGO;
 extern bob_class_t BOB_CLASS_FS;
@@ -23,6 +24,7 @@ extern bob_class_t BOB_CLASS_PKG_CONFIG;
 extern bob_class_t BOB_CLASS_PLATFORM;
 
 static bob_class_t* const BOB_CLASSES[] = {
+	&BOB_CLASS_AQUARIUM,
 	&BOB_CLASS_CC,
 	&BOB_CLASS_CARGO,
 	&BOB_CLASS_FS,
