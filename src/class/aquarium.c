232320b605bc1ed9e190ea2eebaf4b8275268e74
Aymeric Wibo <obiwac@gmail.com>
2024-12-25 20:51:50 +0100

    AquariumBuilder: Installing to another aquarium

--- src/class/aquarium.c
+++ src/class/aquarium.c
@@ -13,26 +13,28 @@
 #include <assert.h>
 #include <inttypes.h>
 
+#include "aquarium_common.h"
+
 #define AQUARIUM "Aquarium"
 #define MAGIC (strhash(AQUARIUM "() magic"))
-
-typedef struct {
-	char* template;
-	char* kernel;
-} state_t;
 
 static size_t aquarium_count = 0;
 
 static int create_step(size_t data_count, void** data) {
 	for (size_t i = 0; i < data_count; i++) {
-		state_t* const state = data[i];
+		aquarium_state_t* const state = data[i];
 		size_t id = aquarium_count++;
 
 		// Generate the cookie.
 
-		char* STR_CLEANUP cookie = NULL;
-		asprintf(&cookie, "%s/bob/aquarium.cookie.%zu.aquarium", out_path, id);
-		assert(cookie != NULL);
+		asprintf(&state->cookie, "%s/bob/aquarium.cookie.%zu.aquarium", out_path, id);
+		assert(state->cookie != NULL);
+
+		// If the cookie already exists, remove it.
+
+		if (access(state->cookie, F_OK) == 0) {
+			remove(state->cookie);
+		}
 
 		// Create the aquarium.
 
@@ -47,18 +49,18 @@ static int create_step(size_t data_count, void** data) {
 		}
 
 		cmd_add(&cmd, "create");
-		cmd_add(&cmd, cookie);
+		cmd_add(&cmd, state->cookie);
 
 		cmd_set_redirect(&cmd, false); // So we can get progress if a template needs to be downloaded e.g.
-		int rv = cmd_exec(&cmd);
+		int const rv = cmd_exec(&cmd);
 
 		if (rv == 0) {
-			set_owner(cookie);
+			set_owner(state->cookie);
 		}
 
-		cmd_log(&cmd, cookie, state->template, "create aquarium", "created aquarium", true);
+		cmd_log(&cmd, NULL, state->template, "create aquarium", "created aquarium", true);
 
-		if (rv != 0) {
+		if (rv < 0) {
 			return -1;
 		}
 	}
@@ -66,7 +68,7 @@ static int create_step(size_t data_count, void** data) {
 	return 0;
 }
 
-static int add_kernel(state_t* state, flamingo_arg_list_t* args, flamingo_val_t** rv) {
+static int add_kernel(aquarium_state_t* state, flamingo_arg_list_t* args, flamingo_val_t** rv) {
 	assert(args->count == 1);
 
 	if (args->args[0]->kind != FLAMINGO_VAL_KIND_STR) {
@@ -80,7 +82,7 @@ static int add_kernel(state_t* state, flamingo_arg_list_t* args, flamingo_val_t*
 	return 0;
 }
 
-static int prep_image(state_t* state, flamingo_arg_list_t* args, flamingo_val_t** rv) {
+static int prep_image(aquarium_state_t* state, flamingo_arg_list_t* args, flamingo_val_t** rv) {
 	assert(args->count == 0);
 
 	// TODO Imaging build step.
@@ -91,7 +93,7 @@ static int prep_image(state_t* state, flamingo_arg_list_t* args, flamingo_val_t*
 static int call(flamingo_val_t* callable, flamingo_arg_list_t* args, flamingo_val_t** rv, bool* consumed) {
 	*consumed = true;
 
-	state_t* const state = callable->owner->owner->inst.data; // TODO Should this be passed to the call function of a class?
+	aquarium_state_t* const state = callable->owner->owner->inst.data; // TODO Should this be passed to the call function of a class?
 
 	if (flamingo_cstrcmp(callable->name, "add_kernel", callable->name_size) == 0) {
 		return add_kernel(state, args, rv);
@@ -106,7 +108,7 @@ static int call(flamingo_val_t* callable, flamingo_arg_list_t* args, flamingo_va
 }
 
 static void free_state(flamingo_val_t* inst, void* data) {
-	state_t* const state = data;
+	aquarium_state_t* const state = data;
 
 	free(state->template);
 	free(state->kernel);
@@ -124,7 +126,7 @@ static int instantiate(flamingo_val_t* inst, flamingo_arg_list_t* args) {
 
 	// Create state object.
 
-	state_t* const state = malloc(sizeof *state);
+	aquarium_state_t* const state = malloc(sizeof *state);
 	assert(state != NULL);
 
 	state->template = strndup(args->args[0]->str.str, args->args[0]->str.size);
