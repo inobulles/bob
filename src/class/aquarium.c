f496ee7ed70cee23edf9a13912da9381fad6a7d1
Aymeric Wibo <obiwac@gmail.com>
2024-12-27 14:41:49 +0100

    Aquarium: Add `Aquarium.exec()`

--- src/class/aquarium.c
+++ src/class/aquarium.c
@@ -21,6 +21,11 @@
 
 typedef struct {
 	aquarium_state_t* state;
+	char* cmd;
+} exec_bss_t;
+
+typedef struct {
+	aquarium_state_t* state;
 	char* cookie;
 } image_bss_t;
 
@@ -75,6 +80,27 @@ static int create_step(size_t data_count, void** data) {
 	return 0;
 }
 
+static int exec_step(size_t data_count, void** data) {
+	for (size_t i = 0; i < data_count; i++) {
+		exec_bss_t* const bss = data[i];
+
+		LOG_INFO("%s" CLEAR ": Executing command on aquarium...", bss->state->template);
+
+		cmd_t CMD_CLEANUP cmd = {0};
+		cmd_create(&cmd, "aquarium", "enter", bss->state->cookie, NULL);
+
+		asprintf(&cmd.pending_stdin, "export HOME=/root\n%s\n", bss->cmd);
+		assert(cmd.pending_stdin != NULL);
+
+		cmd_set_redirect(&cmd, false); // It's nice to see these logs.
+		cmd_exec(&cmd);                // XXX Return values don't matter to us here, commands can fail.
+
+		cmd_log(&cmd, NULL, bss->state->template, "execute command on aquarium", "executed command on aquarium", true);
+	}
+
+	return 0;
+}
+
 static int image_step(size_t data_count, void** data) {
 	for (size_t i = 0; i < data_count; i++) {
 		image_bss_t* const bss = data[i];
@@ -114,6 +140,31 @@ static int add_kernel(aquarium_state_t* state, flamingo_arg_list_t* args, flamin
 	return 0;
 }
 
+static size_t exec_count = 0;
+
+static int prep_exec(aquarium_state_t* state, flamingo_arg_list_t* args) {
+	assert(args->count == 1);
+	flamingo_val_t* const arg = args->args[0];
+
+	if (arg->kind != FLAMINGO_VAL_KIND_STR) {
+		LOG_FATAL(AQUARIUM ": Expected argument to be string");
+		return -1;
+	}
+
+	char* const cmd = strndup(arg->str.str, arg->str.size);
+
+	// Add build step to execute on the aquarium.
+	// Order absolutely does matter here.
+
+	exec_bss_t* const bss = malloc(sizeof *bss);
+	assert(bss != NULL);
+
+	bss->state = state;
+	bss->cmd = cmd;
+
+	return add_build_step(MAGIC ^ strhash(__func__) + exec_count++, "Executing command on aquarium", exec_step, bss);
+}
+
 static int prep_image(aquarium_state_t* state, flamingo_arg_list_t* args, flamingo_val_t** rv) {
 	assert(args->count == 0);
 
@@ -137,6 +188,10 @@ static int call(flamingo_val_t* callable, flamingo_arg_list_t* args, flamingo_va
 
 	if (flamingo_cstrcmp(callable->name, "add_kernel", callable->name_size) == 0) {
 		return add_kernel(state, args, rv);
+	}
+
+	else if (flamingo_cstrcmp(callable->name, "exec", callable->name_size) == 0) {
+		return prep_exec(state, args);
 	}
 
 	else if (flamingo_cstrcmp(callable->name, "image", callable->name_size) == 0) {
