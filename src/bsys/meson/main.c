b7525c329f2fffb836e04f7f2d8680d1d86428a9
Aymeric Wibo <obiwac@gmail.com>
2025-01-06 22:12:52 +0100

    bsys/meson: Installation

--- src/bsys/meson/main.c
+++ src/bsys/meson/main.c
@@ -26,8 +26,12 @@ static int build(void) {
 
 	LOG_INFO("Meson setup...");
 
+	char* STR_CLEANUP prefix = NULL;
+	asprintf(&prefix, "-Dprefix=%s", install_prefix);
+	assert(prefix != NULL);
+
 	cmd_t CMD_CLEANUP cmd = {0};
-	cmd_create(&cmd, "meson", "setup", meson_path);
+	cmd_create(&cmd, "meson", "setup", meson_path, prefix, NULL);
 	cmd_set_redirect(&cmd, false);
 
 	if (cmd_exec(&cmd) < 0) {
@@ -57,9 +61,33 @@ static int build(void) {
 	return 0;
 }
 
+static int install(void) {
+	char* STR_CLEANUP meson_path = NULL;
+	asprintf(&meson_path, "%s/meson", out_path);
+	assert(meson_path != NULL);
+
+	LOG_SUCCESS("Ninja install...");
+
+	cmd_t CMD_CLEANUP cmd = {0};
+	cmd_create(&cmd, "ninja", "-C", meson_path, "install", NULL);
+	cmd_set_redirect(&cmd, false);
+
+	if (cmd_exec(&cmd) < 0) {
+		LOG_FATAL("Ninja install failed.");
+		return -1;
+	}
+
+	else {
+		LOG_SUCCESS("Ninja install succeeded.");
+	}
+
+	return 0;
+}
+
 bsys_t const BSYS_MESON = {
 	.name = "Meson",
 	.key = "meson",
 	.identify = identify,
 	.build = build,
+	.install = install,
 };
