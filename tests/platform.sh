3e9e4bc403df572ce03a10e72639d82bfea062df
Aymeric Wibo <obiwac@gmail.com>
2025-08-31 15:57:44 +0200

    tests: Test for `Platform.getenv()`

--- tests/platform.sh
+++ tests/platform.sh
@@ -7,10 +7,21 @@
 # Test Platform.os().
 # Should be the exact same output as uname(1).
 
-rm -rf tests/platform/.bob
-out=$(bob -C tests/platform build 2>&1)
+rm -rf tests/platform_os/.bob
+out=$(bob -C tests/platform_os build 2>&1)
 
 if [ "$out" != "$(uname)" ]; then
 	echo "Platform.os() doesn't match uname(1) ('$out' vs '$(uname)')." >&2
+	exit 1
+fi
+
+# Test Platform.getenv().
+# Should be the exact same output as $PATH.
+
+rm -rf tests/platform_getenv/.bob
+out=$(bob -C tests/platform_getenv build 2>&1)
+
+if [ "$out" != "$PATH" ]; then
+	echo "Platform.getenv(\"PATH\") doesn't match \$PATH ('$out' vs '$PATH')." >&2
 	exit 1
 fi
