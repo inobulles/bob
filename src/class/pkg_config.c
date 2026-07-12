2e2ad3be0559360be355d75fe42f8538d7eb9735
Aymeric Wibo <me@obiw.ac>
2026-07-12 16:50:46 +0200

    PkgConfig: Make evaluation a build step and use `PkgConfigCookie` class

--- src/class/pkg_config.c
+++ src/class/pkg_config.c
@@ -1,8 +1,9 @@
 // SPDX-License-Identifier: MIT
-// Copyright (c) 2025 Aymeric Wibo
+// Copyright (c) 2025-2026 Aymeric Wibo
 
 #include <common.h>
 
+#include <build_step.h>
 #include <class/class.h>
 #include <cmd.h>
 #include <logging.h>
@@ -11,41 +12,21 @@
 #include <assert.h>
 #include <string.h>
 
+#include <class/pkg_config.h>
+
 #define PKG_CONFIG "PkgConfig"
+
+typedef struct {
+	pkg_config_cookie_t* cookie;
+	char* module;
+	char const* fn;
+	char const* flag;
+} bss_t;
 
 static flamingo_val_t* cflags_val = NULL;
 static flamingo_val_t* libs_val = NULL;
 
-static int common(
-	char const* fn,
-	char const* flag,
-	flamingo_arg_list_t* args,
-	flamingo_val_t** rv
-) {
-	// Validate arguments.
-
-	if (args->count != 1) {
-		LOG_FATAL(PKG_CONFIG ".%s: Expected 1 argument, got %zu.", fn, args->count);
-		return -1;
-	}
-
-	flamingo_val_t* const arg = args->args[0];
-
-	if (arg->kind != FLAMINGO_VAL_KIND_STR) {
-		LOG_FATAL(PKG_CONFIG ".%s: Expected module argument to be a string.", fn);
-		return -1;
-	}
-
-	char* STR_CLEANUP module = strndup(arg->str.str, arg->str.size);
-	assert(module != NULL);
-
-	// Check if pkg-config is available.
-
-	if (!cmd_exists("pkg-config")) {
-		LOG_FATAL(PKG_CONFIG ": Couldn't find 'pkg-config' executable in PATH. pkg-config is something you must install separately.");
-		return -1;
-	}
-
+static int eval(char const* fn, char const* flag, char* module, pkg_config_cookie_t* cookie) {
 	// Run command.
 
 	cmd_t CMD_CLEANUP cmd = {0};
@@ -60,7 +41,8 @@ static int common(
 
 	if (cmd_rv != 0) {
 		LOG_ERROR(PKG_CONFIG ".%s: pkg-config failed:", fn, orig_out);
-		printf("%s", orig_out);
+		fprintf(stderr, "%s", orig_out);
+		free(module);
 		return -1;
 	}
 
@@ -88,8 +70,96 @@ static int common(
 		vec->vec.elems[vec->vec.count++] = flamingo_val_make_cstr(tok);
 	}
 
-	*rv = vec;
+	cookie->out_vec = vec;
+	free(module);
+
 	return 0;
+}
+
+static int eval_step(size_t data_count, void** data) {
+	// XXX If we wanted to, I think we could make this a pool without too much difficulty. But cba rn.
+
+	for (size_t i = 0; i < data_count; i++) {
+		bss_t* const bss = data[i];
+
+		if (eval(bss->fn, bss->flag, bss->module, bss->cookie) < 0) {
+			return -1;
+		}
+	}
+
+	return 0;
+}
+
+static void free_cookie(flamingo_val_t* inst, void* data) {
+	pkg_config_cookie_t* const cookie = data;
+
+	if (cookie->out_vec != NULL) {
+		flamingo_val_decref(cookie->out_vec);
+	}
+
+	free(cookie);
+}
+
+static int common(
+	char const* fn,
+	char const* flag,
+	flamingo_arg_list_t* args,
+	flamingo_val_t** rv
+) {
+	// Validate arguments.
+
+	if (args->count != 2) {
+		LOG_FATAL(PKG_CONFIG ".%s: Expected 2 arguments, got %zu.", fn, args->count);
+		return -1;
+	}
+
+	flamingo_val_t* const module = args->args[0];
+
+	if (module->kind != FLAMINGO_VAL_KIND_STR) {
+		LOG_FATAL(PKG_CONFIG ".%s: Expected module argument to be a string.", fn);
+		return -1;
+	}
+
+	flamingo_val_t* const cookie_val = args->args[1];
+
+	if (cookie_val->kind != FLAMINGO_VAL_KIND_INST) {
+		LOG_FATAL(PKG_CONFIG ".%s: Expected cookie argument to be an instance of Cookie.", fn);
+		return -1;
+	}
+
+	// Check if pkg-config is available.
+
+	if (!cmd_exists("pkg-config")) {
+		LOG_FATAL(PKG_CONFIG ": Couldn't find 'pkg-config' executable in PATH. pkg-config is something you must install separately.");
+		return -1;
+	}
+
+	// Add our data to the cookie.
+
+	pkg_config_cookie_t* const cookie = malloc(sizeof *cookie);
+	assert(cookie_val != NULL);
+
+	cookie->out_vec = NULL;
+
+	cookie_val->inst.data = cookie;
+	cookie_val->inst.free_data = free_cookie;
+
+	// Add build step.
+	// It is safe to merge multiple pkg-config evaluation build steps.
+	// XXX Technically, don't need to strdup() 'fn' and 'flag' cuz will be in .rodata.
+
+	bss_t* const bss = malloc(sizeof *bss);
+	assert(bss != NULL);
+
+	bss->module = strndup(module->str.str, module->str.size);
+	;
+	assert(bss->module != NULL);
+
+	bss->cookie = cookie;
+	bss->fn = fn;
+	bss->flag = flag;
+
+	return add_build_step(((uint64_t) 'pkg-' << 32) | 'conf', "pkg-config evaluation", eval_step, bss);
 }
 
 static int get_cflags(flamingo_arg_list_t* args, flamingo_val_t** rv) {
@@ -116,11 +186,11 @@ static int call(flamingo_val_t* callable, flamingo_arg_list_t* args, flamingo_va
 }
 
 static void populate(char* key, size_t key_size, flamingo_val_t* val) {
-	if (flamingo_cstrcmp(key, "cflags", key_size) == 0) {
+	if (flamingo_cstrcmp(key, "_cflags", key_size) == 0) {
 		cflags_val = val;
 	}
 
-	if (flamingo_cstrcmp(key, "libs", key_size) == 0) {
+	if (flamingo_cstrcmp(key, "_libs", key_size) == 0) {
 		libs_val = val;
 	}
 }
---
+++ src/class/pkg_config.h
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+// Copyright (c) 2026 Aymeric Wibo
+
+#include <flamingo/flamingo.h>
+
+typedef struct {
+	flamingo_val_t* out_vec;
+} pkg_config_cookie_t;
